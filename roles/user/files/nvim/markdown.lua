f53dcf0ec4869256093d000b9520da725ecd310d
Xander Harris <xandertheharris@gmail.com>
2025-10-17 18:27:18 -0700

    Fix errors
    Closes #302
    
    Update pipfile, lock
    
    Closes #320
    
    Fix 3.10 errors
    
    Closes #302
    
    Drop instant markdown plugin
    
    Closes #320

--- roles/user/files/nvim/markdown.lua
+++ roles/user/files/nvim/markdown.lua
@@ -1,8 +1,7 @@
 return {
-  { "tadmccorkle/markdown.nvim" },
-  { "vim-pandoc/vim-pandoc" },
-  { "vim-pandoc/vim-pandoc-syntax" },
-  { "suan/vim-instant-markdown" },
-  { "tpope/vim-markdown" },
-  { "plasticboy/vim-markdown" },
+	{ "tadmccorkle/markdown.nvim" },
+	{ "vim-pandoc/vim-pandoc" },
+	{ "vim-pandoc/vim-pandoc-syntax" },
+	{ "tpope/vim-markdown" },
+	{ "plasticboy/vim-markdown" },
 }
